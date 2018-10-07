e3e0490e112e7732a60df3a00fd7d1832e430c30
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 11:13:57 +0530

    adding SquareToStepLineStage class

---
+++ SquareToStepLineStage.ts
@@ -0,0 +1,31 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+class SquareToStepLineStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : SquareToStepLineStage = new SquareToStepLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
