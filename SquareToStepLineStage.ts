5ba3a60bca6d2c09a5d507ac3e0ab792a43f9fde
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 11:27:19 +0530

    addong STSLNode class

--- SquareToStepLineStage.ts
+++ SquareToStepLineStage.ts
@@ -71,3 +71,64 @@ class Animator {
         }
     }
 }
+
+class STSLNode {
+    next : STSLNode
+    prev : STSLNode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new STSLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        context.strokeStyle = '#283593'
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        const scale = this.state.scale
+        const gap = w / (nodes + 1)
+        const size = gap / 3
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for(var j = 0; j < 2; j++) {
+            const sc = Math.min(0.5, Math.max(0, scale - 0.5 * j)) * 2
+            const sf = 1 - 2 * j
+            context.save()
+            context.scale(sf, 1)
+            context.beginPath()
+            context.moveTo(0, -size)
+            context.lineTo(-size * (1 - sc), -size)
+            context.lineTo(-size * (1 - sc), size)
+            context.lineTo(0, size)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : STSLNode {
+        var curr : STSLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
